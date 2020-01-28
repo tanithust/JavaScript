fb6050c5c16273c2ba7c06f84b38f4748c148d46
Tan Phan <tanithust1202@gmail.com>
2020-01-28 20:29:21 +0700

    add testcase.html and array.js

---
+++ array.js
@@ -0,0 +1,5 @@
+var sport=['football','badminton','volleyball'];
+var x=sport.length;
+var y=sport.sort();
+document.getElementById("length").innerHTML=x;
+document.getElementById("sort").innerHTML=y;
