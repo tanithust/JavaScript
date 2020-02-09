233c7bc15f444015ccbe285b18ca7551089051a8
Tan Phan Xuan <57738033+tanithust@users.noreply.github.com>
2020-02-09 22:48:23 +0700

    some practice

---
+++ script.js
@@ -0,0 +1,14 @@
+var input="javascriptloops";
+var vowel=[],consonant=[];
+var arr=input.split("");
+var i;
+for(i=0;i<arr.length;i++)
+{
+    if (arr[i]=='a'|| arr[i]=='i'||arr[i]=='e'||arr[i]=='o'||arr[i]=='u') 
+        vowel.push(arr[i]);
+    else consonant.push(arr[i]);
+}   
+for(i=0;i<vowel.length;i++){
+    console.log(vowel[i]);
+}
+
